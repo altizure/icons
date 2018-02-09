d63989e79f53a56063b6363b545cd7126b45f5a4
Sam <you@example.com>
2018-02-10 01:01:51 +0800

    * changed the middle stroke to transparent in logo

--- src/icons/altizurelogo.js
+++ src/icons/altizurelogo.js
@@ -6,20 +6,28 @@ const AltizureLogo = ({ size = 600, round = false, bg = '#323232' }) => (
     xmlns='http://www.w3.org/2000/svg'
     width={size}
     height={size}
-    viewBox='0 0 490 490'
-  >
+    viewBox='0 0 490 490'>
     <defs>
-      <linearGradient id='linearGradient' x1='102' y1='291.5' x2='210' y2='291.5' gradientUnits='userSpaceOnUse'>
-        <stop offset='0' stopColor='#fff' stopOpacity='0' />
-        <stop offset='1' stopOpacity='0.8' />
+      <linearGradient
+        id='MIDDLE_STROKE'
+        gradientUnits='userSpaceOnUse'
+        x1='387.821' y1='390.0587' x2='473.402' y2='390.3572'
+        gradientTransform='matrix(1,0,0,-1,0,768)'>
+        <stop offset='0' style={{ 'stop-color': '#ffffff' }} />
+        <stop offset='1' style={{ 'stop-color': '#ffffff', 'stop-opacity': 0 }} />
       </linearGradient>
     </defs>
     {bg &&
-      <rect fill={bg} width='490' height='490' rx={round ? '490' : '36'} ry={round ? '490' : '36'} />
+      <rect width='490' height='490'
+        fill={bg}
+        rx={round ? 490 : 36}
+        ry={round ? 490 : 36} />
     }
-    <path fill='#fff' d='M527,511,382,393c-9-7-10-21-3-29L562.91,160c4.09-5,13.09-7,19.94,1L742.34,361.58C750,370,748,384,737,390L551,512C542,518,534,517,527,511Zm0-145s11-113,10-118-5,0-5,0S435,356,432,361s3,5,3,5Zm-93,31s-8,0-5,6,80,62,80,62,8,7,10,0,6-68,6-68Z' transform='translate(-317 -90)' />
-    <polygon fill='url(#linearGradient)' points='102 307 208 307 210 276 102 276 102 307' />
-    <path fill='#69a3d5' d='M555,324s42,7,58-21c11-24,28-41,53-37,18,21,78,97,78,97s12,16-7,28c-15.05,9.51-188,123-188,123s-6,4-12,2C537,502,555,324,555,324Z' transform='translate(-317 -90)' />
+    <g transform='translate(-262.07226,-91.08877)'>
+      <path fill='#ffffff' d='M688.3,361.6L528.8,161c-6.8-8-15.8-6-19.9-1L325,364c-7,8-6,22,3,29l145,118c7,6,15,7,24,1l186-122C694,384,696,370,688.3,361.6z M473,366l-2,31l-0.3,5c-1,14.7-4,57.2-5.7,63c-2,7-10,0-10,0s-77-56-80-62c-2.6-5.2,3-5.9,4.6-6c-1.1-8.4-4-31.9-1.6-36c3-5,100-113,100-113s4-5,5,0S473,366,473,366z' />
+      <path fill='#69a3d5' d='M501,324c0,0,42,7,58-21c11-24,28-41,53-37c18,21,78,97,78,97s12,16-7,28c-15.1,9.5-188,123-188,123s-6,4-12,2C483,502,501,324,501,324z' />
+      <path fill='url(#MIDDLE_STROKE)' d='M473,366l-2,31h-91c0,0-3-0.5-5,1.8c-2,2.2,0.3,5.1,1.7,6.2s-5.7,2-5.7,2l-24-29l24-29l7.9,10.8L378,361c0,0-3,5,3,5c2,0,4,0,7.5-0.1c18.5,0.1,44.6,0,44.6,0L473,366z' />
+    </g>
   </svg>
 )
 
